13a5f18e67acd6017d0e2ddf442e94fb933cd035
Paxton1337 <paxtonevens@gmail.com>
2020-08-02 16:20:02 +0300

    change structure and add router with lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/profile',
+    pathMatch: 'full'
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./modules/profile/components/profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: 'directions',
+    loadChildren: () => import('./modules/directions/components/directions/directions.module').then(m => m.DirectionsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
