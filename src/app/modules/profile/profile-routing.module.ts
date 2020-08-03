2540546a79c66487b93dd24fdd7cd17f3ccc43ac
stih07 <artur.momo@gmail.com>
2020-08-03 19:36:30 +0300

    Add users

--- src/app/modules/profile/profile-routing.module.ts
+++ src/app/modules/profile/profile-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [{ path: '', component: ProfileComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '1', pathMatch: 'full' },
+  { path: ':id', component: ProfileComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
