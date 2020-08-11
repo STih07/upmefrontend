3e559e203c65f0edc19028a40dc8f8a375bde7fe
Artyom Kazlanzhy <kazlanzhy.artyom@gmail.com>
2020-08-11 12:24:11 +0300

    Moved layout from app-component to an own component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/profile',
-    pathMatch: 'full'
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)
-  },
-  {
-    path: 'directions',
-    loadChildren: () => import('./modules/directions/directions.module').then(m => m.DirectionsModule)
-  },
+    path: '', component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/profile',
+        pathMatch: 'full'
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)
+      },
+      {
+        path: 'directions',
+        loadChildren: () => import('./modules/directions/directions.module').then(m => m.DirectionsModule)
+      },
+    ]
+  }
 ];
 
 @NgModule({
