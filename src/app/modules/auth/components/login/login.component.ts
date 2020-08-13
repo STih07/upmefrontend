5610d1c71ed0870318a939c6d5fda5db4845367d
Artyom Kazlanzhy <kazlanzhy.artyom@gmail.com>
2020-08-13 12:37:50 +0300

    Added show/hide password logic

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -10,15 +10,22 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent {
 
+  showPassword: boolean;
+
   singInForm: FormGroup = this.fb.group({
     email: ['', [Validators.email, Validators.required]],
     password: ['', [Validators.minLength(8), Validators.required]]
   });
 
   constructor(private authService: AuthService, private fb: FormBuilder) {
+    this.showPassword = false;
   }
 
   onSubmit() {
     this.authService.login(this.singInForm.value);
+  }
+
+  onEyeClick() {
+    this.showPassword = !this.showPassword;
   }
 }
