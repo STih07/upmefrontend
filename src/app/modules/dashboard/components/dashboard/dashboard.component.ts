d086ad4df3d6205cc1204014e6665771d6e033d9
Gleb <glebrutskiy2001@gmail.com>
2020-08-17 19:11:26 +0300

    add model

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -7,6 +7,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  users = [
+    {
+      id: 1,
+      name: 'Yuliia Chudina',
+      image: 'assets/img/user_card_img.png',
+      begin: '26/02/2020',
+      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+      formTask: 3,
+      letterTask: 3,
+      testTasks: [
+        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+        { status: 3, name: 'PHP' },
+        { status: 3, name: 'SQL' }
+      ],
+      practiceTasks: [
+        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+        { status: 3, name: 'Front-end' },
+        { status: 3, name: 'Design' }
+      ],
+      decision: 'Принят',
+      finish: '15/03/2020',
+      hasSelected: 1,
+    },
+    {
+      id: 2,
+      name: 'Darrell Web',
+      image: 'assets/img/user_card_img.png',
+      begin: '06/02/2020',
+      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+      formTask: 3,
+      letterTask: 3,
+      testTasks: [
+        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+        { status: 3, name: 'PHP' },
+        { status: 3, name: 'SQL' }
+      ],
+      practiceTasks: null,
+      decision: 'Принят на испытательный',
+      finish: '17/03/2020',
+      hasSelected: 1,
+    },
+    {
+      id: 3,
+      name: 'Lily Bell',
+      image: 'assets/img/user_card_img.png',
+      begin: '01/02/2020',
+      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+      formTask: 3,
+      letterTask: 1,
+      testTasks: [
+        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+        { status: 1, name: 'PHP' },
+        { status: 1, name: 'SQL' }
+      ],
+      practiceTasks: [
+        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+        { status: 1, name: 'Front-end' },
+        { status: 1, name: 'Design' }
+      ],
+      decision: 'Принят на испытательный',
+      finish: '17/03/2020',
+      hasSelected: 0,
+    },
+    {
+      id: 4,
+      name: 'Cody Cooper',
+      image: 'assets/img/user_card_img.png',
+      begin: '06/02/2020',
+      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+      formTask: 3,
+      letterTask: 3,
+      testTasks: [
+        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+        { status: 3, name: 'PHP' },
+        { status: 2, name: 'SQL' }
+      ],
+      practiceTasks: null,
+      decision: 'Принят на испытательный',
+      finish: '17/03/2020',
+      hasSelected: 0,
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
