ee3b376efea3db667bcc1e7563a5e0731e0dfb6f
Gleb <glebrutskiy2001@gmail.com>
2020-08-21 18:05:52 +0300

    Model for Direction

--- src/app/modules/directions/components/directions/directions.component.ts
+++ src/app/modules/directions/components/directions/directions.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectionsComponent implements OnInit {
 
+  works = {
+    total: 68,
+    inProgress: 8,
+    checked: 12
+  };
+
+  users = [
+    {
+      id: 1, // ???
+      name: 'Yuliia Chudina',
+      icon: 'assets/img/user_card_img.png',
+      source: 'https://www.linkedin.com/in/gleb-rutskiy-990ab21a8/',
+      stage: 'Онбординг',
+      // 1 - awaiting, 2 - in progress, 3 - checked
+      status: 3,
+      // 1 - no passed, 2 - with misses, 3 - good, 4 - excellent
+      rating: 4, // ???
+      // tslint:disable-next-line:max-line-length
+      feedBack: 'Кандидат хорошо владеет базовыми знаниями (типы переменнных и операции над ними), небольшой опыт владения классами, льём водичку для красоты, чтобы текста было много, очень много' // ???
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
