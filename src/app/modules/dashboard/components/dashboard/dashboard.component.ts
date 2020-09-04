94e5f161dc6139955f98b145bd55d0159c759be5
Gleb <glebrutskiy2001@gmail.com>
2020-09-04 10:47:04 +0300

    dashboard now support model

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -13,18 +13,18 @@ export class DashboardComponent implements OnInit {
       name: 'Yuliia Chudina',
       image: 'assets/img/user_card_img.png',
       begin: '26/02/2020',
-      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-      formTask: 3,
-      letterTask: 3,
+      // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+      formTask: 2,
+      letterTask: 2,
       testTasks: [
-        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-        { status: 3, name: 'PHP' },
-        { status: 3, name: 'SQL' }
+        // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+        { status: 2, name: 'PHP' },
+        { status: 2, name: 'SQL' }
       ],
       practiceTasks: [
-        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-        { status: 3, name: 'Front-end' },
-        { status: 3, name: 'Design' }
+        // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+        { status: 2, name: 'Front-end' },
+        { status: 2, name: 'Design' }
       ],
       decision: 'Принят',
       finish: '15/03/2020',
@@ -34,13 +34,13 @@ export class DashboardComponent implements OnInit {
       name: 'Darrell Web',
       image: 'assets/img/user_card_img.png',
       begin: '06/02/2020',
-      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-      formTask: 3,
-      letterTask: 3,
+      // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+      formTask: 2,
+      letterTask: 2,
       testTasks: [
-        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-        { status: 3, name: 'PHP' },
-        { status: 3, name: 'SQL' }
+        // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+        { status: 2, name: 'PHP' },
+        { status: 2, name: 'SQL' }
       ],
       practiceTasks: null,
       decision: 'Принят на испытательный',
@@ -51,38 +51,38 @@ export class DashboardComponent implements OnInit {
       name: 'Lily Bell',
       image: 'assets/img/user_card_img.png',
       begin: '01/02/2020',
-      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-      formTask: 3,
-      letterTask: 1,
+      // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+      formTask: 2,
+      letterTask: 0,
       testTasks: [
-        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-        { status: 1, name: 'PHP' },
-        { status: 1, name: 'SQL' }
+        // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+        { status: 0, name: 'PHP' },
+        { status: 0, name: 'SQL' }
       ],
       practiceTasks: [
-        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
+        // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
         { status: 1, name: 'Front-end' },
         { status: 1, name: 'Design' }
       ],
-      decision: 'Принят на испытательный',
-      finish: '17/03/2020',
+      decision: 'Не принят',
+      finish: '--',
     },
     {
       id: 4,
       name: 'Cody Cooper',
       image: 'assets/img/user_card_img.png',
       begin: '06/02/2020',
-      // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-      formTask: 3,
-      letterTask: 3,
+      // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+      formTask: 2,
+      letterTask: 2,
       testTasks: [
-        // 1 ==> don't begin, 2 ==> in progress, 3 ==> complete
-        { status: 3, name: 'PHP' },
-        { status: 2, name: 'SQL' }
+        // 0 ==> don't begin, 1 ==> in progress, 2 ==> complete
+        { status: 2, name: 'PHP' },
+        { status: 1, name: 'SQL' }
       ],
       practiceTasks: null,
-      decision: 'Принят на испытательный',
-      finish: '17/03/2020',
+      decision: 'Не принят, еще очень юн, советуем пойти в it2school',
+      finish: '--',
     },
   ];
 
