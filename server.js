d7857ebe87044d47d0d976ed510f92eb866bfeeb
STih07 <artur.momo@gmail.com>
2020-10-05 23:41:20 +0300

    Test new message

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/upme'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/upme/index.html'));
+  res.send('Hello friend');
+  // res.sendFile(path.join(__dirname+'/dist/upme/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
