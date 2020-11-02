a1bad9fb9046ad33cbbffb83930a89b615987e19
Paxton1337 <paxtonevens@gmail.com>
2020-11-02 13:10:03 +0200

    Feature/RF-77 Added additional information on hover user in Resident page

--- src/app/modals/submit-modal/submit-modal.component.spec.ts
+++ src/app/modals/submit-modal/submit-modal.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SubmitModalComponent } from './submit-modal.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 
 describe('SubmitModalComponent', () => {
@@ -12,7 +13,8 @@ describe('SubmitModalComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SubmitModalComponent],
-      providers: [NgbActiveModal]
+      imports: [ToastrModule.forRoot()],
+      providers: [NgbActiveModal, ToastrService]
     })
       .compileComponents();
   }));
@@ -29,6 +31,6 @@ describe('SubmitModalComponent', () => {
   });
 
   it('should create', () => {
-        expect(component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
