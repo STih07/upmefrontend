5df70b56e1985bcfe1a429fd28bd92e7d108ce0e
Raiden2071 <66531600+Raiden2071@users.noreply.github.com>
2021-02-12 12:04:44 +0200

    m

--- src/app/modals/edit-profile-modal/edit-profile-modal.component.ts
+++ src/app/modals/edit-profile-modal/edit-profile-modal.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ChangepassComponent } from 'src/app/modules/auth/components/changepass/changepass.component';
 
 @Component({
   selector: 'app-edit-profile-modal',
@@ -13,11 +14,12 @@ export class EditProfileModalComponent {
   editProfileForm: FormGroup = this.fb.group({
     fullName: ['', [Validators.pattern('^[a-zа-яA-ZА-Я]+(([\' -][a-zа-яA-ZА-Я ])?[aa-zа-яA-ZА-Я]*)*$'), Validators.required]],
     email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.minLength(5), Validators.required]],
     telNumber: ['+38', [Validators.pattern('^\\+(?:[0-9] ?){6,14}[0-9]$'), Validators.required]],
     telegramNickName: ['']
   });
 
-  constructor(private modalService: NgbActiveModal, private fb: FormBuilder, private router: Router) {
+  constructor(private modalService: NgbActiveModal,private modalService2: NgbModal, private fb: FormBuilder, private router: Router) {
   }
 
   logOut(): void {
@@ -29,5 +31,10 @@ export class EditProfileModalComponent {
 
   onCancel(): void {
     this.modalService.close();
+  }
+
+  open() {
+    const modalRef = this.modalService2.open(ChangepassComponent, {centered: true});
+    modalRef.componentInstance.name = 'World';
   }
 }
--- src/app/modules/auth/components/changepass/changepass.component.ts
+++ src/app/modules/auth/components/changepass/changepass.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { MustMatch } from '../../../../utils/validators/must-match.validator';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'upme-changepass',
@@ -9,10 +13,61 @@ import { Router } from '@angular/router';
 })
 export class ChangepassComponent implements OnInit {
 
-  constructor(private authService: AuthService,private router: Router) {}
+  showPassword: boolean;
+  showErrors: boolean;
 
-  ngOnInit(): void {
+  singUpForm: FormGroup = this.fb.group({
+    password: ['', [Validators.minLength(8), Validators.required]],
+    confirmPassword: ['', [Validators.required]]
+  }, {
+    validator: MustMatch('password', 'confirmPassword')
+  });
+
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+    private activeModal: NgbActiveModal,
+    private modal: NgbModal
+  ) { }
+
+  ngOnInit() {
+    this.showPassword = false;
+    this.showErrors = false;
+    this.singUpForm.valueChanges.subscribe(() => this.showErrors = false);
   }
-  
 
+  onSubmit() {
+    if (this.singUpForm.valid) {
+      this.authService.register(this.singUpForm.value).subscribe(() => this.router.navigateByUrl(''), error => {
+        this.showErrors = true;
+        this.handleError(error);
+      });
+    } else {
+      console.log(this.singUpForm);
+      this.showErrors = true;
+    }
+  }
+
+  onEyeClick() {
+    this.showPassword = !this.showPassword;
+  }
+
+  close(): void {
+    this.activeModal.close();
+    console.log('+');
+  }
+
+  goRecovery() {
+    this.modal.dismissAll();
+    this.router.navigateByUrl('auth/recovery-password');
+  }
+
+  private handleError(error: HttpErrorResponse): void {
+    switch (error.error.message) {
+      case 'Not unique email':
+        this.singUpForm.controls.email.setErrors({ notUniqueEmail: true });
+        break;
+    }
+  }
 }
